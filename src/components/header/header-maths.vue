a1fac61abd78fc205aa07407247f9b0819a21382
kondrieger <kondrieger@gmail.com>
2021-07-05 20:25:42 +0300

    Добавил бургер модалку

--- src/components/header/header-maths.vue
+++ src/components/header/header-maths.vue
@@ -32,17 +32,61 @@
                     </a>
                 </div>
             </div>
-            <div v-else class="header__burger-wrap" @click="burger">
+            <div v-else class="header__burger-wrap" @click="openModal('Burger')">
                 <img class="header__burger-img" :src="require('../../img/svg/burger_icon.svg')" alt="" />
             </div>
         </div>
+        <modal :close="closeModal" v-if="isModalOpen('Burger')">
+            <div slot="header">
+                <img
+                    class="header__logo--white"
+                    :src="require('../../img/svg/maths_logo_white.svg')"
+                    alt="Алгоритмика математика логотип"
+                />
+            </div>
+            <div slot="body">
+                <div class="header__burger-wrap">
+                    <div class="header__links">
+                        <a class="header__links-link" href="#"><p>Факультет программирования</p></a>
+                        <a class="header__links-link" href="#"><p>Франшиза</p></a>
+                        <a class="header__links-link" href="#"><p>Договор оферты</p></a>
+                        <a class="header__links-link" href="#"><p>Контакты</p></a>
+                    </div>
+                    <div class="header__links header__links--media">
+                        <a class="header__links-phone" href="tel:+74950239428"><p>+7 (495) 023-94-28</p></a>
+                        <div class="header__links-social">
+                            <a href="#" class="header__links-social-item">
+                                <img
+                                    class="header__links-social-item-img--whats"
+                                    :src="require('../../img/svg/whats_logo_white.svg')"
+                                    alt="Whatsapp логотип"
+                                />
+                            </a>
+
+                            <a href="#" class="header__links-social-item">
+                                <img
+                                    class="header__links-social-item-img--tg"
+                                    :src="require('../../img/svg/tg_logo_white.svg')"
+                                    alt="Telegram логотип"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </modal>
     </div>
 </template>
 
 <script>
 import './styles.css';
+import Modal from '../modal/modal.vue';
+import modalMixin from '../../scripts/modalMixin.js';
+
 export default {
     name: 'a-header',
+    components: { Modal },
+    mixins: [modalMixin],
     data() {
         return {};
     },
---
+++ src/components/modal/modal.vue
@@ -0,0 +1,92 @@
+<template>
+    <div @click="onClose" class="modal-mask">
+        <div ref="wrapper" class="modal-wrapper modal-wrapper--fullscreen" tabindex="0" @click.stop @keydown="keyDown">
+            <div
+                class="modal-container popup"
+                :class="{ [`popup--${type}`]: type, [`popup--${name}`]: name }"
+                @mousedown="onPopupMouseDown"
+                @mouseup="onPopupMouseUp"
+            >
+                <div class="modal-header popup__header">
+                    <slot name="header">
+                        <!-- Default header -->
+                    </slot>
+                </div>
+
+                <div class="modal-body popup__body">
+                    <slot name="body">
+                        <!-- Default body -->
+                    </slot>
+                </div>
+
+                <img
+                    @click="close"
+                    class="modal-default-button popup__close"
+                    :src="require('../../img/svg/cross.svg')"
+                    alt=""
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import './modal.css';
+
+export default {
+    name: 'modal',
+    props: {
+        type: {
+            type: String,
+        },
+        name: {
+            type: String,
+        },
+        closeOnBtn: {
+            type: Boolean,
+            default: true,
+        },
+        isScrollLocked: {
+            type: Boolean,
+            default: true,
+        },
+        close: {
+            type: Function,
+            default: () => {},
+        },
+    },
+    data() {
+        return {
+            clickInside: false,
+            lock: false,
+        };
+    },
+    methods: {
+        onPopupMouseDown() {
+            this.clickInside = true;
+        },
+        onPopupMouseUp() {
+            this.clickInside = false;
+        },
+        onClose() {
+            if (!this.clickInside) this.close();
+            else this.clickInside = false;
+        },
+        keyDown(e) {
+            switch (e.key) {
+                case 'Escape':
+                    if (this.closeOnBtn) this.close();
+                    e.preventDefault();
+                    break;
+            }
+        },
+    },
+    mounted() {
+        this.lock = this.isScrollLocked;
+        this.$refs.wrapper.focus();
+    },
+    beforeDestroy() {
+        this.lock = false;
+    },
+};
+</script>
